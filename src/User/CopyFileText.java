fd60bbe73a9c51be646d8dace5206efe0d087acf
Trungdeptrai99 <ktdarkness99@gmail.com>
2020-07-27 21:28:06 +0700

    Bài tập copy file

--- src/User/CopyFileText.java
+++ src/User/CopyFileText.java
@@ -1,23 +1,49 @@
 package User;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class CopyFileText {
     public static void main(String[] args) {
+        int count = 0;
+        File original;
+        FileWriter output;
+        Scanner sc = new Scanner(System.in);
         try {
-            File original = new File("src/User/trungdeptrai.txt");
+            original = new File("Data/trungdeptrai3.txt");
+            if(!original.exists()){
+                System.out.println("Không có file đầu vào! Bạn có muốn tạo file mới?");
+                System.out.println("1.Tạo mới");
+                System.out.println("2.Thoát chương trình!");
+                int option = sc.nextInt();
+                switch (option){
+                    case 1:{
+                        original.createNewFile();
+                    }
+                    case 2:{
+                        System.exit(0);
+                    }
+                }
+            }
             FileReader fileReader= new FileReader(original);
             BufferedReader reader = new BufferedReader(fileReader);
-            FileWriter output = new FileWriter("data/last.txt");
-            String line = null;
-            while ((line= reader.readLine())!=null){
-                System.err.println(line);
-                output.write(line+"\n");
-            }
-            output.close();
-            reader.close();
+            output = new FileWriter("Data/Last.txt");
+                int line;
+                while ((line = reader.read() )!=-1) {
+                    if(line==13||line==10){
+                        count--;
+                    }
+                    System.out.println(line);
+                    output.write((char) line );
+                    count++;
+                }
+                output.close();
+                reader.close();
         }catch (Exception e){
             e.printStackTrace();
+        }
+        finally {
+            System.out.println("Số ký tự đã copy là: "+count);
         }
     }
 }
